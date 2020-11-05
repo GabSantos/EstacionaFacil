ff255f613d254c179d1405658c6d548953cdc62d
Gabriel Santos <gabrogolt@gmail.com>
2020-11-05 19:10:58 -0300

    Styles

--- src/components/navigation/Stack.js
+++ src/components/navigation/Stack.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {createStackNavigator} from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import TelaLogin from '../../pages/TelaLogin'
 
 const Stack = createStackNavigator()
 
-export default function StackFunc (props){
-    return(
-        <Stack.Navigator initialRouteName="Login"
-            screenOptions={{headerShown: false}}> 
-            <Stack.Screen name="Login" 
-                component={TelaLogin}>
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+export default function StackFunc(props) {
+  return (
+    <Stack.Navigator initialRouteName="Login"
+      screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login"
+        component={TelaLogin}>
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
--- src/pages/TelaLogin.js
+++ src/pages/TelaLogin.js
@@ -1,33 +1,144 @@
-import React,{useState} from 'react'
-import {Text, View, TextInput, Button, StyleSheet, ImageBackground} from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native'
+import { loadAsync } from 'expo-font'
+import { AppLoading } from 'expo'
 
 import background from '../../assets/fundotelainicial.png'
 
-export default function Busca (props){
+const fetchFonts = () => {
+  return loadAsync({
+    'Stardos': require('../../assets/fonts/StardosStencil-Bold.ttf'),
+    'Modak': require('../../assets/fonts/Modak-Regular.ttf')
+  })
+}
 
-    return(
-        <View style={styles.container}>
-            <ImageBackground source={background} style={styles.bg}>
-                <View>
+export default function TelaLogin(props) {
+  const [dataLoaded, setDataLoaded] = useState(false)
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
 
-                </View>
-                <View>
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    )
+  }
 
-                </View>
-            </ImageBackground>
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={background} style={styles.bg}>
+        <View style={styles.topv}>
+          <Text style={styles.estaciona}>
+            ESTACIONA
+                    </Text>
+          <Text style={styles.facil}>
+            FÁCIL
+          </Text>
         </View>
-    );
+        <View style={styles.botv}>
+          <View style={styles.inputs}>
+            <TextInput
+              placeholder='E-mail'
+              placeholderTextColor='#fbfbfb'
+              value={email}
+              onChange={email => setEmail(email)}
+              style={styles.inputText}
+            />
+          </View>
+          <View style={styles.inputs}>
+            <TextInput
+              placeholder='Senha'
+              placeholderTextColor='#fbfbfb'
+              value={senha}
+              onChange={senha => setSenha(senha)}
+              style={styles.inputText}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.botao}
+            onPress={
+              () => {
+              }
+            }
+          >
+            <Text style={styles.botaoText}>
+              Entrar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        height: '100%'
-    },
-    bg: {
-        width: '100%',
-        height: '100%'
-    }
+  container: {
+    width: '100%',
+    height: '100%',
+    padding: 0,
+    margin: 0
+  },
+  bg: {
+    width: '100%',
+    height: '100%'
+  },
+  topv: {
+    flex: 1,
+    width: '100%',
+    padding: 0,
+    margin: 0,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  botv: {
+    flex: 1,
+    width: '100%',
+    padding: 0,
+    margin: 0,
+    alignItems: 'center',
+  },
+  estaciona: {
+    fontFamily: 'Stardos',
+    fontSize: 60,
+    color: '#FBFBFB'
+  },
+  facil: {
+    fontFamily: 'Stardos',
+    fontSize: 60,
+    marginLeft: 150,
+    color: '#FBFBFB'
+  },
+  inputs: {
+    height: 54,
+    width: 280,
+    borderColor: '#FBFBFB',
+    borderWidth: 1,
+    borderRadius: 40,
+    justifyContent: 'center',
+    margin: 20
+  },
+  inputText: {
+    height: 54,
+    width: 244,
+    marginLeft: 18,
+    fontSize: 20,
+  },
+  botao: {
+    backgroundColor: 'linear-gradient(90deg, rgba(255, 514, 0, 1) 100%, rgba(250,255,0,1) 100%)',
+    width: 192,
+    height: 38,
+    borderRadius: 40,
+    marginTop: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  botaoText: {
+    fontFamily: 'Modak',
+    fontSize: 30,
+    color: '#0085FF',
+  }
+
 })
 
