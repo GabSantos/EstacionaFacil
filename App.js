bd07c332146d95c394f57d9f3cbef6ecdfde4662
thamisrissil <thamis.rissi@anexsdev.com.br>
2020-11-08 19:57:55 -0300

    ggrg

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import Telainicial from './src/TelaInicial'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Telainicial></Telainicial>
     </View>
   );
 }
